f66c95f92afcd1fb659cc4f6fe87fbb1addf414e
Anjali Gautam <gautamanjali569@gmail.com>
2025-01-12 11:04:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@
         setTimeout(function () {
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
-                
             }
         }, 1);
     };
